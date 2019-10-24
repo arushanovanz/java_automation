03579c27843801de64ad31aa1d265075599613ac
arushanovanz <nadin.spb@gmail.com>
2019-10-24 15:49:07 +0300

    дабавлены функции для вычисления площади

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,32 @@
 package ru.stqa.pft.sandbox;
 
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("hello, world!");
+    hello("Alexei");
+    hello("user");
+    hello("bspb.qa");
+
+    double len = 5;
+    double a = 5;
+    double b = 4;
+
+    System.out.println("Площадь квадрата со стороной " + len + " равна = " + area(len));
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна = " + area(a, b));
+  }
+
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("hello " + somebody + "!");
   }
 }
+
