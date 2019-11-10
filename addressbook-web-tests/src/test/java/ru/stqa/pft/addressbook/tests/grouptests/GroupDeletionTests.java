7f8466c56a34cf287c196f34c705e1c6e16a8b4d
arushanovanz <nadin.spb@gmail.com>
2019-11-10 13:11:34 +0300

    Refactored selectGroup method by index

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/grouptests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/grouptests/GroupDeletionTests.java
@@ -13,7 +13,7 @@ public class GroupDeletionTests extends TestBase {
     if (!app.getGroupHelper().isThereAGroup()) {
         app.getGroupHelper().createGroup(new GroupData("name3","header3","footer3"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().deleteSelectedGroups();
     int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after, before-1);
